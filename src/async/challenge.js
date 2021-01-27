768e8a29788acd65401bd9ce709654f0ec5cd700
Vidal NuñezG <ncsreload@gmail.com>
2021-01-26 19:18:38 -0500

    initializing project with fetch PokeApi

--- src/async/challenge.js
+++ src/async/challenge.js
@@ -3,12 +3,11 @@ const API = 'https://rickandmortyapi.com/api/character/'
 
 const anotherFunction = async (url_api) => {
   try {
-    const data = await fetchData(API)
+    const data = await fetchData(url_api)
     const origin = await fetchData(data.results[0].origin.url)
 
 
     console.log(data.info.count)
-    console.log(data.results[0].name)
     console.log(origin.dimension)
   } catch (error) {
     console.error(error)
